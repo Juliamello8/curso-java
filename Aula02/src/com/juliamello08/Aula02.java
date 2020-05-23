9272337ab9c070fcdf34e377155874d01fb26db5
Juliamello08 <juliainline@gmail.com>
2020-05-23 15:34:24 -0300

    Classe Caneta-Aula2

---
+++ Aula02/src/com/juliamello08/Aula02.java
@@ -0,0 +1,13 @@
+package com.juliamello08;
+
+public class Aula02 {
+	
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta();
+		c1.carga = 1;
+		c1.cor = "Azul";
+		c1.ponta = 0.5f;
+		c1.tampada = false;
+	}
+
+}
---
+++ Aula02/src/com/juliamello08/Caneta.java
@@ -0,0 +1,23 @@
+package com.juliamello08;
+
+public class Caneta {
+	String modelo;
+	String cor;
+	float ponta;
+	int carga;
+	boolean tampada;
+	
+	void status() {
+		System.out.println("Uma caneta de cor " + this.cor );
+		System.out.println("Está tampada? " + this.tampada );
+	}
+	void rabiscar() {
+		
+	}
+	void tampar() {
+		
+	}
+	void destampar() {
+		
+	}
+}
